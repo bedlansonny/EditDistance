2d24152b9f5ef5704cb6d92ddeedbc5f24d52acb
bedlansonny <bedlansonny@gmail.com>
2018-05-13 10:10:07 -0500

    exports table and traces steps

--- src/EditDistance.java
+++ src/EditDistance.java
@@ -3,23 +3,82 @@ import java.util.*;
 
 public class EditDistance
 {
+    static PrintWriter fileWriter;
+    static int[][] d;
+    static String a;
+    static String b;
+
     public static void main(String args[]) throws IOException
     {
         Scanner in = new Scanner(new File("input.txt"));
+        fileWriter = new PrintWriter(new File("output.csv"));
 
         int c = in.nextInt();
         for(int ci = 0; ci < c; ci++)
         {
-            String a = in.next();
-            String b = in.next();
-            System.out.println(distance(a,b));
+            a = in.next();
+            b = in.next();
+            System.out.println(distance());
+            exportData();
+            System.out.println(trace() + "\n");
         }
+        fileWriter.close();
     }
 
-    static int distance(String a, String b)     //b is x, a is y
+    static ArrayDeque<String> trace()
+    {
+        int r = d.length-1;
+        int c = d[0].length-1;
+        String temp = a;
+        ArrayDeque<String> steps = new ArrayDeque<>();
+        steps.addLast(temp);
+
+        while(r > 0 || c > 0)
+        {
+            if(r > 0 && c > 0 && a.charAt(r-1) == b.charAt(c-1))
+            {
+                //no change
+                r = r-1;
+                c = c-1;
+            }
+            else if(c > 0 && d[r][c-1] == d[r][c] - 1)
+            {
+                //insert
+                if(r > temp.length())
+                    temp = temp + b.charAt(c-1);
+                else
+                    temp = temp.substring(0,r)+b.charAt(c-1)+temp.substring(r);
+                steps.addLast(temp);
+                c = c-1;
+            }
+            else if(r > 0 && d[r-1][c] == d[r][c] - 1)
+            {
+                //delete
+                temp = temp.substring(0,r-1)+temp.substring(r);
+                steps.addLast(temp);
+                r = r-1;
+            }
+            else if(r > 0 && c > 0 && d[r-1][c-1] == d[r][c] - 1)
+            {
+                //replace
+                temp = temp.substring(0,r-1)+b.charAt(c-1)+temp.substring(r);
+                steps.addLast(temp);
+                r = r-1;
+                c = c-1;
+            }
+            else
+            {
+                System.out.println("\n\n\n\nERROR - UNHANDLED CASE\n\n\n\n");
+            }
+        }
+
+        return steps;
+    }
+
+    static int distance() throws IOException
     {
         //memo table
-        int[][] d = new int[a.length()+1][b.length()+1];
+        d = new int[a.length()+1][b.length()+1];
 
         //base case - filling top row
         for(int bi = 0; bi < d[0].length; bi++)
@@ -41,5 +100,27 @@ public class EditDistance
         }
 
         return d[d.length-1][d[0].length-1];
+    }
+
+    static void exportData() throws IOException //for testing
+    {
+        fileWriter.print(",'',");
+        for(int i = 0; i < b.length(); i++)
+            fileWriter.print(b.charAt(i)+",");
+        fileWriter.println();
+
+        fileWriter.print("'',");
+        for(int i = 0; i < d[0].length; i++)
+            fileWriter.print(d[0][i]+",");
+        fileWriter.println();
+
+        for(int r = 1; r < d.length; r++)
+        {
+            fileWriter.print(a.charAt(r-1)+",");
+            for(int c = 0; c < d[0].length; c++)
+                fileWriter.print(d[r][c]+",");
+            fileWriter.println();
+        }
+        fileWriter.println();
     }
 }
